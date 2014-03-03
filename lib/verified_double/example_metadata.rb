5c849ec82b28d47a1bacbde6250d888076960427
Guo Xiang Tan <tgx_world@hotmail.com>
2014-03-03 13:42:57 -0800

    Better error handling for contract description.

--- lib/verified_double/example_metadata.rb
+++ lib/verified_double/example_metadata.rb
@@ -3,10 +3,11 @@ module VerifiedDouble
     def described_class
       if description.is_a?(Module)
         description
-      elsif description.is_a?(String)
+      elsif description.is_a?(String) && !metadata[:example_group].nil?
         ExampleMetadata.new(metadata[:example_group]).described_class
       else
-        raise
+        raise StandardError,
+          'VerifiedDouble class description is invalid'
       end
     end
 
@@ -17,8 +18,9 @@ module VerifiedDouble
     def method_signature_string
       if description =~ /^[\.\#]/
         description
-      elsif description.nil?
-        raise
+      elsif description.nil? || metadata[:example_group].nil?
+        raise StandardError,
+          'VerifiedDouble contract spec description is invalid'
       else
         ExampleMetadata.new(metadata[:example_group]).method_signature_string
       end
